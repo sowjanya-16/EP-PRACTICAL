8ed4d46ea4acb7420a09ddab86acec8654f560f7
sowjanya-16 <74000502+sowjanya-16@users.noreply.github.com>
2021-01-21 19:46:11 +0530

    Update DBUtil.java

--- experiment-2/DBUtil.java
+++ experiment-2/DBUtil.java
@@ -5,12 +5,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 public class DBUtil {
 	final static String forNameURL="driver:com.mysql.jdbc.Driver";
-	final static String dBURL="jdbc:mysql:thin:@localhost:3306:\"ep\"";
+	final static String dBURL="jdbc:mysql:thin:@localhost:3306\ep";
 	final static String username="root";
 	final static String password="root";
 	public static Connection DBConnection() throws SQLException, ClassNotFoundException {
 		Class.forName(forNameURL);
-		Connection con=DriverManager.getConnection(dBURL,username,password);
+		Connection con=java.sql.DriverManager.getConnection(dBURL,username,password);
 		return con;
 	}
  
