e52fc7618c7211e0028c308c88025ca96c002ac7
sowjanya-16 <74000502+sowjanya-16@users.noreply.github.com>
2021-01-21 19:48:20 +0530

    Update StudentInsertion.java

--- experiment-2/StudentInsertion.java
+++ experiment-2/StudentInsertion.java
@@ -4,17 +4,26 @@ import com.klu.Bean.StudentBean;
 import com.klu.Connections.DBUtil;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import java.sql.ResultSet;
 public class StudentInsertion {
      public int StudentInsert(StudentBean student) throws ClassNotFoundException, SQLException{
     	Connection con=DBUtil.DBConnection();
-    	try {
-    		PreparedStatement ps=con.prepareStatement("Insert into student values(student)");
-    		return 1;
-    	}
-    	catch(SQLException sql)
-    	{
-    	 return 0;
-    	}
-    } 
+    		PreparedStatement ps=con.prepareStatement("Insert into student values(?,?,?)");
+    		ps.setInt(1, student.getRegno());
+    		ps.setString(2, student.getName());
+    		ps.setString(3, student.getEmail());
+    		int i=ps.executeUpdate();
+    		System.out.println(i+"records inserted");
+    		return i;
+     }
+    	
+    	public void StudentDisplay(StudentBean student) throws ClassNotFoundException, SQLException{
+        	Connection con=DBUtil.DBConnection();
+        	PreparedStatement ps=con.prepareStatement("Select * from StudentBean");
+        	ResultSet rs=ps.executeQuery();
+        	while(rs.next())
+        	{
+        		System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+        	}
+    }
 }
